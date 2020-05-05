5d00d8479fba249b4c8f0686129f60e462c492e5
Oleksandr Denysov <denisovalexsd@gmail.com>
2020-05-06 00:18:52 +0300

    Done

--- src/main/java/com/example/appium/PageView.java
+++ src/main/java/com/example/appium/PageView.java
@@ -10,7 +10,9 @@ import org.openqa.selenium.support.PageFactory;
 public class PageView {
     AppiumDriver<MobileElement> driver;
 
-    // TODO define textField MobileElement using @FindBy kind of annotations for iOS and Android
+    @AndroidFindBy(id = "edit")
+    @iOSXCUITFindBy(iOSNsPredicate = "name == 'IntegerA'")
+    private MobileElement textField;
 
     public PageView(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
@@ -18,12 +20,11 @@ public class PageView {
     }
 
     public String getTextField() {
-        // TODO return text from the textField element
-        return "";
+        return this.textField.getText();
     }
 
     public PageView setTextField(String text) {
-        // TODO set text to the textField element
+        this.textField.sendKeys(text);
         return this;
     }
 }
